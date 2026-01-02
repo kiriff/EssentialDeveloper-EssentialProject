168572ed2b93fa7245ed22863c263a7ef9d619a8
Kirill Filipov <kiriffoff@gmail.com>
2026-01-03 01:04:03 +0300

    Move HTTPClient to its own file

---
+++ EssentialFeed/EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Kirill Filipov on 1/3/26.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
